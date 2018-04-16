13c3239adca4d0ca164c7661232854c0e40a5614
NataliiaPrivezentseva <nataliia.privezentseva@gmail.com>
2018-04-16 21:15:39 +0200

    First commit

---
+++ app/src/main/java/com/example/asterisk/xmastoys/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.asterisk.xmastoys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.FloatingActionButton;
+import android.os.Bundle;
+import android.widget.GridView;
+
+import com.example.asterisk.xmastoys.adapter.ToyAdapter;
+import com.example.asterisk.xmastoys.model.Toy;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //todo заменить на другой вид коллекции
+        final ArrayList<Toy> toyCollection = new ArrayList<>();
+        toyCollection.add(new Toy("Такси изобилия", "2017/2018",
+                "За этой игрушкой пришлось ходить трижды: первые два раза киоск на ярмарке" +
+                        "уже закрылся. А на третий раз игрушки на витрине не оказалось. Думала, " +
+                        "что закончились... А на самом деле продавец отложила для меня последний" +
+                        "экземпляр.", R.drawable.taxi));
+        toyCollection.add(new Toy("Не такси", "2016/2017",
+                "А тут должна быть другая история", R.drawable.taxi));
+        toyCollection.add(new Toy("Третья игрушка", "2016/2017",
+                "А тут должна быть другая история", R.drawable.taxi));
+
+        GridView gridview = findViewById(R.id.grid_view);
+        gridview.setAdapter(new ToyAdapter(this, toyCollection));
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+//        gridview.setOnItemClickListener(new OnItemClickListener() {
+//            public void onItemClick(AdapterView<?> parent, View v,
+//                                    int position, long id) {
+//                Toast.makeText(HelloGridView.this, "" + position,
+//                        Toast.LENGTH_SHORT).show();
+//            }
+//        });
+    }
+}
---
+++ app/src/main/java/com/example/asterisk/xmastoys/model/Toy.java
@@ -0,0 +1,55 @@
+package com.example.asterisk.xmastoys.model;
+
+public class Toy {
+
+    private String mToyName;
+
+    private String mYear;
+
+    private String mStory;
+
+    private int mImageResourceId;
+
+    public Toy(String toyName, String year) {
+        this.mToyName = toyName;
+        this.mYear = year;
+    }
+
+    public Toy(String toyName, String year, String story, int imageResourceId) {
+        this(toyName, year);
+        this.mStory = story;
+        this.mImageResourceId = imageResourceId;
+    }
+
+    public String getmToyName() {
+        return mToyName;
+    }
+
+    public void setmToyName(String mToyName) {
+        this.mToyName = mToyName;
+    }
+
+    public String getmYear() {
+        return mYear;
+    }
+
+    public void setmYear(String mYear) {
+        this.mYear = mYear;
+    }
+
+    public String getmStory() {
+        return mStory;
+    }
+
+    public void setmStory(String mStory) {
+        this.mStory = mStory;
+    }
+
+    public int getmImageResourceId() {
+        return mImageResourceId;
+    }
+
+    public void setmImageResourceId(int mImageResourceId) {
+        this.mImageResourceId = mImageResourceId;
+    }
+}
