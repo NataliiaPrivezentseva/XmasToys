8589b569e446529d20d7a95c185b3230da34a015
Nataliia Privezentseva <nataliia.privezentseva@gmail.com>
2018-06-06 12:32:07 +0200

    Change amount of columns depending on orientation changes

--- app/src/main/java/com/example/asterisk/xmastoys/MainActivity.java
+++ app/src/main/java/com/example/asterisk/xmastoys/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.asterisk.xmastoys;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
@@ -35,13 +37,30 @@ public class MainActivity extends AppCompatActivity {
                 "А тут должна быть другая история", R.drawable.taxi));
         toyCollection.add(new Toy("Третья игрушка", "2016/2017",
                 "А тут должна быть другая история", R.drawable.taxi));
+        toyCollection.add(new Toy("Четвертая игрушка", "2016/2017",
+                "А тут должна быть другая история", R.drawable.taxi));
+        toyCollection.add(new Toy("Пятая игрушка", "2016/2017",
+                "А тут должна быть другая история", R.drawable.taxi));
+        toyCollection.add(new Toy("Шестая игрушка", "2016/2017",
+                "А тут должна быть другая история", R.drawable.taxi));
+        toyCollection.add(new Toy("Седьмая игрушка", "2016/2017",
+                "А тут должна быть другая история", R.drawable.taxi));
 
         //todo прописать логику кнопки
         FloatingActionButton fab = findViewById(R.id.add_toy_fab);
 
         RecyclerView myrv = findViewById(R.id.my_recycler_view);
         ToyRecyclerAdapter myAdapter = new ToyRecyclerAdapter(this,toyCollection);
-        myrv.setLayoutManager(new GridLayoutManager(this,2));
+
+        int amountOfColumns;
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+            amountOfColumns = 2;
+        }
+        else{
+            amountOfColumns = 3;
+        }
+        myrv.setLayoutManager(new GridLayoutManager(this, amountOfColumns));
+
         myrv.setAdapter(myAdapter);
     }
 }
