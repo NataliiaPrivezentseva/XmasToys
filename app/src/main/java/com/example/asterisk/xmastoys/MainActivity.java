d964e90221eb5bc04c50ecab0bff0492e9bad9ea
Nataliia Privezentseva <nataliia.privezentseva@gmail.com>
2018-06-06 14:28:14 +0200

    Improve appearance of the grid

--- app/src/main/java/com/example/asterisk/xmastoys/MainActivity.java
+++ app/src/main/java/com/example/asterisk/xmastoys/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.asterisk.xmastoys;
 
 import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
+import android.view.View;
 
 import com.example.asterisk.xmastoys.adapter.ToyRecyclerAdapter;
 import com.example.asterisk.xmastoys.model.Toy;
@@ -22,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
-        if (myToolbar != null){
+        if (myToolbar != null) {
             setSupportActionBar(myToolbar);
         }
 
@@ -50,17 +53,60 @@ public class MainActivity extends AppCompatActivity {
         FloatingActionButton fab = findViewById(R.id.add_toy_fab);
 
         RecyclerView myrv = findViewById(R.id.my_recycler_view);
-        ToyRecyclerAdapter myAdapter = new ToyRecyclerAdapter(this,toyCollection);
+        ToyRecyclerAdapter myAdapter = new ToyRecyclerAdapter(this, toyCollection);
 
         int amountOfColumns;
-        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
             amountOfColumns = 2;
-        }
-        else{
+        } else {
             amountOfColumns = 3;
         }
         myrv.setLayoutManager(new GridLayoutManager(this, amountOfColumns));
-
+        myrv.addItemDecoration(new GridItemSpacingDecoration(amountOfColumns, pdToPx(10), false));
         myrv.setAdapter(myAdapter);
     }
+
+    /**
+     * Converting dp to pixel
+     */
+    private int pdToPx(int dp) {
+        Resources r = getResources();
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
+    }
+
+    class GridItemSpacingDecoration extends RecyclerView.ItemDecoration {
+
+        private int spanCount;
+        private int spacing;
+        private boolean includeEdge;
+
+        public GridItemSpacingDecoration(int spanCount, int spacing, boolean includeEdge) {
+            this.spanCount = spanCount;
+            this.spacing = spacing;
+            this.includeEdge = includeEdge;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            int position = parent.getChildAdapterPosition(view); // item position
+            int column = position % spanCount; // item column
+
+            if (includeEdge) {
+                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
+                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
+
+                if (position < spanCount) { // top edge
+                    outRect.top = spacing;
+                }
+                outRect.bottom = spacing; // item bottom
+            } else {
+                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
+                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
+                if (position >= spanCount) {
+                    outRect.top = spacing; // item top
+                }
+            }
+        }
+    }
+
 }
