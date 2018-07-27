92e6144cc8411e46a970f58a96592f4e123b137a
Nataliia Privezentseva <nataliia.privezentseva@gmail.com>
2018-07-27 11:12:03 +0200

    Add validation for e-mail and password that user provides

--- app/src/main/java/com/example/asterisk/xmastoys/EmailLoginActivity.java
+++ app/src/main/java/com/example/asterisk/xmastoys/EmailLoginActivity.java
@@ -23,9 +23,10 @@ public class EmailLoginActivity extends AppCompatActivity {
 
     private FirebaseAuth auth;
     private EditText inputEmail;
-//    private EditText inputPassword;
     private android.support.design.widget.TextInputEditText inputPassword;
     private ProgressDialog progressDialog;
+    private TextView eMailError;
+    private TextView passwordError;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -41,7 +42,6 @@ public class EmailLoginActivity extends AppCompatActivity {
 
         // Get Firebase auth instance
         auth = FirebaseAuth.getInstance();
-
 
         inputEmail = findViewById(R.id.log_email_edit_text);
         inputPassword = findViewById(R.id.log_password);
@@ -62,7 +62,6 @@ public class EmailLoginActivity extends AppCompatActivity {
         });
 
         Button logIn = findViewById(R.id.login_button);
-
         logIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -71,31 +70,78 @@ public class EmailLoginActivity extends AppCompatActivity {
         });
 
         Button register = findViewById(R.id.register_button);
-
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 registerUser();
             }
         });
+
+        eMailError = findViewById(R.id.error_email_text_vew);
+        passwordError = findViewById(R.id.error_password_text_vew);
     }
 
-    private void loginUser(){
+    private boolean validateEmail(String email) {
+        // Check if email is empty
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, R.string.enter_email, Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        // Check if e-mail contains proper symbols
+        if (!email.matches("[-\\w]+(\\.[-\\w]+)*@[a-zA-Z0-9]+\\.[a-zA-Z0-9]+")) {
+            Toast.makeText(this, R.string.check_email, Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean validatePassword(String password) {
+        // Check if password is empty
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, R.string.enter_password, Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        // Check password for its length
+        if (password.length() < 6) {
+            Toast.makeText(this, R.string.password_length, Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean validateInputs(String email, String password) {
+        if (!validateEmail(email)) {
+            eMailError.setVisibility(View.VISIBLE);
+            return false;
+        } else {
+            eMailError.setVisibility(View.INVISIBLE);
+        }
+
+        if (!validatePassword(password)) {
+            passwordError.setVisibility(View.VISIBLE);
+            return false;
+        } else {
+            passwordError.setVisibility(View.INVISIBLE);
+        }
+
+        return true;
+    }
+
+    private void loginUser() {
+        // Get email and password from edit texts
         String email = inputEmail.getText().toString().trim();
-        String password  = inputPassword.getText().toString().trim();
+        String password = inputPassword.getText().toString().trim();
 
-        // Check if email and passwords are empty
-        if(TextUtils.isEmpty(email)){
-            Toast.makeText(this,R.string.enter_email,Toast.LENGTH_LONG).show();
+        // Validate inputs
+        if (!validateInputs(email, password)) {
             return;
         }
 
-        if(TextUtils.isEmpty(password)){
-            Toast.makeText(this,R.string.enter_password,Toast.LENGTH_LONG).show();
-            return;
-        }
-
-        // If e-mail and password are not empty, display a progress dialog
+        // If e-mail and password are valid, display a progress dialog
         progressDialog.setMessage(getString(R.string.logging_wait));
         progressDialog.show();
 
@@ -104,16 +150,16 @@ public class EmailLoginActivity extends AppCompatActivity {
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
+                        if (task.isSuccessful()) {
                             //TODO getApplicationContext() in intent?
                             Toast.makeText(EmailLoginActivity.this,
-                                    R.string.successfully_logged_in,Toast.LENGTH_LONG).show();
+                                    R.string.successfully_logged_in, Toast.LENGTH_LONG).show();
                             startActivity(new Intent(getApplicationContext(), MainActivity.class));
                             finish();
                         } else {
                             //display some message here
                             Toast.makeText(EmailLoginActivity.this,
-                                    R.string.login_error,Toast.LENGTH_LONG).show();
+                                    R.string.login_error, Toast.LENGTH_LONG).show();
                             //todo send message to developer
                         }
                         progressDialog.dismiss();
@@ -121,23 +167,15 @@ public class EmailLoginActivity extends AppCompatActivity {
                 });
     }
 
-    private void registerUser(){
+    private void registerUser() {
         // Get email and password from edit texts
         String email = inputEmail.getText().toString().trim();
-        String password  = inputPassword.getText().toString().trim();
+        String password = inputPassword.getText().toString().trim();
 
-        // Check if email and passwords are empty
-        if(TextUtils.isEmpty(email)){
-            Toast.makeText(this, R.string.enter_email,Toast.LENGTH_LONG).show();
+        // Validate inputs
+        if (!validateInputs(email, password)) {
             return;
         }
-
-        if(TextUtils.isEmpty(password)){
-            Toast.makeText(this, R.string.enter_password,Toast.LENGTH_LONG).show();
-            return;
-        }
-
-        //TODO other validations of input
 
         // If e-mail and password are OK, display a progress dialog
         progressDialog.setMessage(getString(R.string.registering_wait));
@@ -149,10 +187,10 @@ public class EmailLoginActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         //todo log results
-                        if(task.isSuccessful()){
+                        if (task.isSuccessful()) {
                             //display some message here
                             Toast.makeText(EmailLoginActivity.this,
-                                    R.string.successfully_registered,Toast.LENGTH_LONG).show();
+                                    R.string.successfully_registered, Toast.LENGTH_LONG).show();
 
                             Bundle infoToSend = new Bundle();
                             infoToSend.putString("eMail", inputEmail.getText().toString());
@@ -165,7 +203,7 @@ public class EmailLoginActivity extends AppCompatActivity {
                         } else {
                             //display some message here
                             Toast.makeText(EmailLoginActivity.this,
-                                    R.string.registration_error,Toast.LENGTH_LONG).show();
+                                    R.string.registration_error, Toast.LENGTH_LONG).show();
                             //todo send message to developer
                         }
                         progressDialog.dismiss();
