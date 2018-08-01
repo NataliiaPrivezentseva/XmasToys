d8b97278cf8a90717078f333b5e94902fbc75c67
Nataliia Privezentseva <nataliia.privezentseva@gmail.com>
2018-08-01 10:44:30 +0200

    Add logic for taking picture from gallery or capturing a new picture and showing it on activity_add_toy layout

--- app/src/main/java/com/example/asterisk/xmastoys/AddToyActivity.java
+++ app/src/main/java/com/example/asterisk/xmastoys/AddToyActivity.java
@@ -1,16 +1,22 @@
 package com.example.asterisk.xmastoys;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -23,12 +29,18 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.io.IOException;
+
 public class AddToyActivity extends AppCompatActivity {
+
+    static final int GALLERY = 0;
+    static final int CAMERA = 1;
 
     EditText newToyName;
     EditText newToyYear;
     EditText newToyStory;
     ImageView defaultPicture;
+    ImageButton addPhoto;
     int newImageResourceId;
 
     Toy newToy;
@@ -64,11 +76,20 @@ public class AddToyActivity extends AppCompatActivity {
         newToyYear = findViewById(R.id.year_edit_text_view);
         newToyStory = findViewById(R.id.story_edit_text_view);
         defaultPicture = findViewById(R.id.default_picture_image_view);
+        addPhoto = findViewById(R.id.camera_image_button);
 
         //TODO for editing an existing toy
-        if (getIntent().getExtras() != null){
+        if (getIntent().getExtras() != null) {
             //todo how to find this toy in collection?
         }
+
+        addPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPictureDialog();
+            }
+        });
+
 
         FloatingActionButton fabAddToy = findViewById(R.id.done_toy_fab);
         fabAddToy.setOnClickListener(new View.OnClickListener() {
@@ -80,10 +101,10 @@ public class AddToyActivity extends AppCompatActivity {
 
                 CollectionReference dbToyCollection = db.collection("users").document(userId).collection("toyCollection");
 
-                if (validateInput(toyName, toyYear)){
+                if (validateInput(toyName, toyYear)) {
                     newToy = new Toy(toyName, toyYear);
 
-                    if (!TextUtils.isEmpty(toyStory)){
+                    if (!TextUtils.isEmpty(toyStory)) {
                         newToy.setmStory(toyStory);
                     }
 
@@ -109,16 +130,16 @@ public class AddToyActivity extends AppCompatActivity {
         });
     }
 
-    public boolean validateInput(String toyName, String toyYear){
+    private boolean validateInput(String toyName, String toyYear) {
         // Check if name of the toy is empty
-        if(TextUtils.isEmpty(toyName)){
+        if (TextUtils.isEmpty(toyName)) {
             Toast.makeText(AddToyActivity.this, "You should provide the name of the toy!",
                     Toast.LENGTH_SHORT).show();
             return false;
         }
 
         // Check if year is empty
-        if(TextUtils.isEmpty(toyYear)){
+        if (TextUtils.isEmpty(toyYear)) {
             Toast.makeText(AddToyActivity.this, "You should provide the year for the toy!",
                     Toast.LENGTH_SHORT).show();
             return false;
@@ -131,11 +152,73 @@ public class AddToyActivity extends AppCompatActivity {
         }
 
         // Check if year length is not a digit
-        if (toyYear.matches("\\D") || toyYear.contains(".")){
+        if (toyYear.matches("\\D") || toyYear.contains(".")) {
             Toast.makeText(AddToyActivity.this, "Year should contain only numbers",
                     Toast.LENGTH_SHORT).show();
             return false;
         }
         return true;
+    }
+
+    private void showPictureDialog() {
+        AlertDialog.Builder pictureDialog = new AlertDialog.Builder(this);
+        pictureDialog.setTitle(R.string.add_photo);
+        pictureDialog.setItems(R.array.photo_choice, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case GALLERY:
+                        choosePhotoFromGallery();
+                        break;
+                    case CAMERA:
+                        takePhotoFromCamera();
+                        break;
+                }
+            }
+        });
+        pictureDialog.show();
+    }
+
+    private void choosePhotoFromGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, GALLERY);
+    }
+
+    private void takePhotoFromCamera() {
+            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(cameraIntent, CAMERA);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_CANCELED) {
+            return;
+        }
+        if (requestCode == GALLERY) {
+            if (data != null) {
+                Uri contentURI = data.getData();
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), contentURI);
+//                    String path = saveImage(bitmap);
+//                    Toast.makeText(AddToyActivity.this, "Image saved!", Toast.LENGTH_SHORT).show();
+                    defaultPicture.setImageBitmap(bitmap);
+
+                } catch (IOException e) {
+                    Log.e("ADD_TOY_FROM_GALLERY", e.getMessage());
+                    Toast.makeText(AddToyActivity.this, R.string.failed_take_picture_from_gallery, Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        } else if (requestCode == CAMERA) {
+            if (data.getExtras() != null) {
+                Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+                defaultPicture.setImageBitmap(thumbnail);
+//            saveImage(thumbnail);
+//            Toast.makeText(AddToyActivity.this, "Image Saved!", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
